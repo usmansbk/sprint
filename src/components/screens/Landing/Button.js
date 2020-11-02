7880ec2a1554546dc37fdda572545fc2e5e7033b
usmansbk <usmansbk@gmail.com>
2020-11-02 03:55:45 +0100

    animate button

--- src/components/screens/Landing/Button.js
+++ src/components/screens/Landing/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useTheme} from '@shopify/restyle';
 import {View, StyleSheet, Animated, TouchableOpacity} from 'react-native';
-import {Text, Icon} from '@components/common';
+import {Icon} from '@components/common';
 
 const SIZE = 60;
 const EXPANDED_WIDTH = 151;
@@ -13,6 +13,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   button: {
+    flexDirection: 'row',
     height: SIZE,
     width: SIZE,
     borderRadius: SIZE / 2,
@@ -20,7 +21,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   text: {
-    color: 'white',
+    fontFamily: 'Demi',
+    fontSize: 18,
+    lineHeight: 43,
   },
 });
 
@@ -51,10 +54,48 @@ export default function AnimatedButton({onPress = () => null, x, index}) {
               }),
             },
           ]}>
-          <Icon name="chevron-right" size={24} />
+          <Animated.View
+            style={{
+              transform: [
+                {
+                  translateX: x.interpolate({
+                    inputRange: [0, index],
+                    outputRange: [0, 50],
+                    extrapolate: 'clamp',
+                  }),
+                },
+              ],
+            }}>
+            <Icon
+              name={index ? 'arrow-right' : 'chevron-right'}
+              size={index ? 14 : 24}
+            />
+          </Animated.View>
         </Animated.View>
       </TouchableOpacity>
-      <Text variant="body2">Go</Text>
+      <Animated.Text
+        disabled
+        style={[
+          styles.text,
+          {
+            color: x.interpolate({
+              inputRange: [0, index],
+              outputRange: [theme.colors.buttonPrimaryBackground, '#fff'],
+              extrapolate: 'clamp',
+            }),
+            transform: [
+              {
+                translateY: x.interpolate({
+                  inputRange: [0, index],
+                  outputRange: [0, -(EXPANDED_HEIGHT - 2)],
+                  extrapolate: 'clamp',
+                }),
+              },
+            ],
+          },
+        ]}>
+        Go
+      </Animated.Text>
     </View>
   );
 }
