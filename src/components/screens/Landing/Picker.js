c886dcbf7d7416e84df99113cd6aac34843d4fd7
usmansbk <usmansbk@gmail.com>
2020-11-02 04:27:57 +0100

    picker icon

--- src/components/screens/Landing/Picker.js
+++ src/components/screens/Landing/Picker.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useTheme} from '@shopify/restyle';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Image} from 'react-native';
 import {Text} from '@components/common';
 
 const options = ['BUY', 'RENT'];
@@ -9,31 +9,55 @@ const styles = StyleSheet.create({
   container: {
     marginHorizontal: 10,
   },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
   divider: {
-    height: 1,
-    marginRight: 6,
+    height: 2,
+    marginRight: 8,
     marginLeft: 4,
+  },
+  icon: {
+    width: 20,
+    height: 20,
+    marginHorizontal: 8,
   },
 });
 
 export default function BigDropDownPicker() {
   const theme = useTheme();
   const [selected, setSelected] = React.useState(options[0]);
+  const [isOpen, setState] = React.useState(false);
+
   return (
     <View style={styles.container}>
-      <Text
-        onPress={() => setSelected(options[1])}
-        variant="landingScreenPrimary">
-        {selected}
-      </Text>
-      <View
-        style={[
-          styles.divider,
-          {
-            backgroundColor: theme.colors.primaryBackground,
-          },
-        ]}
-      />
+      <View style={styles.row}>
+        <View>
+          <Text
+            onPress={() => setSelected(options[1])}
+            variant="landingScreenPrimary">
+            {selected}
+          </Text>
+          <View
+            style={[
+              styles.divider,
+              {
+                backgroundColor: theme.colors.primaryBackground,
+              },
+            ]}
+          />
+        </View>
+        <Image
+          style={styles.icon}
+          resizeMode="contain"
+          source={
+            isOpen
+              ? require('@assets/icons/triangle_up.png')
+              : require('@assets/icons/triangle_down.png')
+          }
+        />
+      </View>
     </View>
   );
 }
