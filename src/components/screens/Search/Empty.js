8bf282117a067ada0c132b24e26ccb9796817c1a
usmansbk <usmansbk@gmail.com>
2020-11-02 07:35:41 +0100

    refactor component

---
+++ src/components/screens/Search/Empty.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {useTheme} from '@shopify/restyle';
+import {Image, View, StyleSheet} from 'react-native';
+import {Text} from '@components/common';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: 154,
+    height: 154,
+  },
+});
+
+export default function Empty() {
+  const theme = useTheme();
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          backgroundColor: theme.colors.mainBackground,
+        },
+      ]}>
+      <Image
+        style={styles.image}
+        source={require('@assets/images/location_pin.png')}
+      />
+      <Text variant="emptySearchTitle">No Location</Text>
+      <Text variant="emptySearchSubtitle">Please enter your location</Text>
+    </View>
+  );
+}
--- src/components/screens/Search/Search.js
+++ src/components/screens/Search/Search.js
@@ -1,36 +1,6 @@
 import React from 'react';
-import {useTheme} from '@shopify/restyle';
-import {Image, View, StyleSheet} from 'react-native';
-import {Text} from '@components/common';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  image: {
-    width: 154,
-    height: 154,
-  },
-});
+import Empty from './Empty';
 
 export default function Search() {
-  const theme = useTheme();
-  return (
-    <View
-      style={[
-        styles.container,
-        {
-          backgroundColor: theme.colors.mainBackground,
-        },
-      ]}>
-      <Image
-        style={styles.image}
-        source={require('@assets/images/location_pin.png')}
-      />
-      <Text variant="emptySearchTitle">No Location</Text>
-      <Text variant="emptySearchSubtitle">Please enter your location</Text>
-    </View>
-  );
+  return <Empty />;
 }
